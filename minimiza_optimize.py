50453efc749cdca14493b9f9fd33bb682cc62a14
metalwii <kamalmqureshi@gmail.com>
2018-09-10 21:31:55 -0400

    Minima of an objective function and Matplot lib graph showing test_run example

--- minimiza_optimize.py
+++ minimiza_optimize.py
@@ -3,8 +3,27 @@ import matplotlib.pyplot as plt
 import numpy as np
 import scipy.optimize as spo
 
-def f(X):
+class Optimize():
 
-    Y = (X - 1.5)**2 + 0.5
-    print('X = {}, Y={}'.format(X,Y))
-    return Y
+    def f(X):
+
+        Y = (X - 1.5)**2 + 0.5
+        print('X = {}, Y={}'.format(X,Y))
+        return Y
+
+    def test_run():
+        Xguess = 2.0
+        min_result = spo.minimize(Optimize.f ,Xguess, method='SLSQP', options={'disp': True})
+        print('Minima found at:')
+        print('X = {}, Y={}'.format(min_result.x, min_result.fun))
+
+        Xplot = np.linspace(0.5,2.5,21)
+        Yplot = Optimize.f(Xplot)
+        plt.plot(Xplot, Yplot)
+        plt.plot(min_result.x, min_result.fun, 'ro')
+        plt.title('Minima of an objective function')
+        plt.show()
+
+
+if __name__ == "__main__":
+    Optimize.test_run()
